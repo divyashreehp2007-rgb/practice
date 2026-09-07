55314795e1d45826280b4a982bd230159b8ba781
divyashree <divyashreehp2007@gmail.com>
2026-09-07 21:01:38 +0530

    hallo.java file created

---
+++ hello.java
@@ -0,0 +1,7 @@
+publiic static void main {
+	Scanner input = new Scanner(System.out);
+	System.out.print("enter the name: ");
+	String name = input.next();
+	system.out.print("hello " + name);
+	input.close();
+}
