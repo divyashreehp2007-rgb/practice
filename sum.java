2691ef2b5f80698ab60309a83c52b2d06a795ae7
divyashree <divyashreehp2007@gmail.com>
2026-09-07 20:45:50 +0530

    sum.java is created

---
+++ sum.java
@@ -0,0 +1,9 @@
+public static void main {
+	Scanner input = new Scanner(System.in)
+	int a = input.nextInt();
+	int b = input.nextInt();
+	int sum = a + b;
+	System.out.print("sum of two number: ");
+	System.out.printf(sum);
+	input.close;
+}
